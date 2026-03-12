0782e6666cc6269c4b71a6398ff7a40c41261d27
mshruthis113 <m.shruthi.s.113@kalvium.community>
2026-03-12 10:14:43 +0530

    Intialize

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import Header from "./components/Header"
-import ProductList from "./components/ProductList"
+import React from 'react';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import { products } from './data/products';
+import './styles/App.css';
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Header />
-      <ProductList />
+      <main className="main-content">
+        <ProductList products={products} />
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
